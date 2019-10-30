5bfca039ee218bc7ce68b80a368d84106da8ecd4
marcos brendon de paula azevedo <marcos.brendon@aluno.ufms.br>
2019-10-30 02:25:28 -0300

    Cliennte Com interface

---
+++ Cliente/src/cliente/Cliente.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cliente;
+
+/**
+ *
+ * @author MarcosBrendon
+ */
+public class Cliente {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        main atual=new main();
+        atual.setVisible(true);
+    }
+    
+}
