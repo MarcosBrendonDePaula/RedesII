91ba10c4bc9e186dacb0dd94664884291387a3ef
marcos brendon de paula azevedo <marcos.brendon@aluno.ufms.br>
2019-11-04 00:18:33 -0300

    adiçao

---
+++ ClienteAndroid/app/src/main/java/com/projeto/redes2client/MainActivity.java
@@ -0,0 +1,77 @@
+package com.projeto.redes2client;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import EasySocket.*;
+
+public class MainActivity extends AppCompatActivity {
+    public EasySocket cliente;
+    public EditText ipporta;
+    public EditText nome;
+    public Button ConnectBtn;
+    public EditText campo;
+    public EditText Texto;
+    public Button EnviarBtn;
+    public Thread Monitora;
+
+    public Runnable Atualizador=new Runnable() {
+        @Override
+        public void run() {
+            while(true){
+                if(cliente.getSocket()==null){
+                    cliente=null;
+                    return;
+                }
+                try {
+                    Thread.sleep(1000);
+                    if(!cliente.Buffer.isEmpty())
+                        campo.append(cliente.getEntrada()+"\n");
+                } catch (Exception ex) {
+                    System.out.println("Erro Thread");
+                }
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+        ipporta = findViewById(R.id.editText);
+        nome = findViewById(R.id.editText3);
+        ConnectBtn = findViewById(R.id.button);
+        campo = findViewById(R.id.editText4);
+        Texto = findViewById(R.id.editText5);
+        EnviarBtn = findViewById(R.id.button2);
+        Monitora = new Thread(Atualizador);
+
+        ConnectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] x = (ipporta.getText()+"").split(":");
+                cliente = new EasySocket(x[0],Integer.parseInt(x[1]),"cliente");
+                cliente.ClientStart();
+                cliente.startVerificador();
+                Monitora.start();
+            }
+        });
+
+        EnviarBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                campo.append("Voce:"+Texto.getText()+"\n");
+                cliente.Enviar(nome.getText()+":"+Texto.getText());
+                Texto.setText("");
+            }
+        });
+    }
+}
